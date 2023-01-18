bc9832d092d0e16c5e82941118c5fee4b0d824f6
Celil <celiloz45@hotmail.com>
2023-01-18 21:32:18 +0300

    Initial commit

---
+++ app/src/main/java/com/oz/artbook/ArtActivity.kt
@@ -0,0 +1,27 @@
+package com.oz.artbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.oz.artbook.databinding.ActivityArtBinding
+
+class ArtActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityArtBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityArtBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+
+    fun saveButtonClicked(view: View) {
+
+    }
+
+    fun selectImage(view: View) {
+
+    }
+}
