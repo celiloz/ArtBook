beb5911032629a9a96551259fe998be84e42fd6b
Celil <celiloz45@hotmail.com>
2023-01-26 23:33:31 +0300

    Permissions added

--- app/src/main/java/com/oz/artbook/ArtActivity.kt
+++ app/src/main/java/com/oz/artbook/ArtActivity.kt
@@ -1,9 +1,17 @@
 package com.oz.artbook
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import com.google.android.material.snackbar.Snackbar
 import com.oz.artbook.databinding.ActivityArtBinding
+import java.util.jar.Manifest
 
 class ArtActivity : AppCompatActivity() {
 
@@ -22,6 +30,18 @@ class ArtActivity : AppCompatActivity() {
     }
 
     fun selectImage(view: View) {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //rationale
+                Snackbar.make(view,"Permisson needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", View.OnClickListener {
 
+                }).show()
+            }else{
+                //request permisson
+            }
+        }else{
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //intent
+        }
     }
 }
