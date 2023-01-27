e007bf32e8074c581fc7d9b19b03b9e63dfcddef
Celil <celiloz45@hotmail.com>
2023-01-27 20:41:59 +0300

    Permission settings updated

--- app/src/main/java/com/oz/artbook/ArtActivity.kt
+++ app/src/main/java/com/oz/artbook/ArtActivity.kt
@@ -1,11 +1,18 @@
 package com.oz.artbook
 
+import android.app.DownloadManager.Request
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PackageManagerCompat
@@ -16,12 +23,14 @@ import java.util.jar.Manifest
 class ArtActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityArtBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedBitmap : Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityArtBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
 
     }
 
@@ -38,10 +47,50 @@ class ArtActivity : AppCompatActivity() {
                 }).show()
             }else{
                 //request permisson
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             }
         }else{
             val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            //intent
+            activityResultLauncher.launch(intentToGallery)
+
+        }
+    }
+    private fun registerLauncher() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intentFromResult = result.data
+                if (intentFromResult != null) {
+                    val imageData = intent.data
+                    //binding.imageView.setImageURI(imageData)
+                    if (imageData != null) {
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28 ) {
+                                var source = ImageDecoder.createSource(
+                                    this@ArtActivity.contentResolver,
+                                    imageData
+                                )
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }else{
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageData)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                    }
+            }
+        }
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                //permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            } else{
+                //permission denied
+                Toast.makeText(this@ArtActivity, "Permission needed!", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
